a489fcd959e78f1df5842d14ffef8621b356f690
tigo <rf.tigas@gmail.com>
2025-05-12 14:27:44 -0300

    add tui with list and completing features

---
+++ cmd/tui/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/vtigo/cli-task-manager/internal/db"
+	"github.com/vtigo/cli-task-manager/internal/handlers"
+	"github.com/vtigo/cli-task-manager/internal/models"
+)
+
+type model struct {
+	tasks  	  []*models.Task
+	cursor    int
+	handler   *handlers.TaskHandler
+}
+
+func initialModel() model {
+	// Initiate managers (tasks, storage)
+	taskManager := models.NewTaskManager()
+	storage := db.NewFileStorage("storage")
+	handler := handlers.NewTaskHandler(taskManager,storage)
+	
+	// Load tasks from storage and add them to taskManager.Tasks
+	tasks, err := storage.LoadTasks()
+	if err != nil {
+		fmt.Println("Failed to load tasks: ", err)
+		os.Exit(1)
+	}
+	for _, t := range tasks {
+		taskManager.Tasks = append(taskManager.Tasks, t)
+	}
+	
+	return model{
+		tasks: taskManager.Tasks,
+		handler: handler,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Initiate model with no commands
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+    switch msg := msg.(type) {
+
+    // Is it a key press?
+    case tea.KeyMsg:
+
+        // Cool, what was the actual key pressed?
+        switch msg.String() {
+
+        // These keys should exit the program.
+        case "ctrl+c", "q":
+            return m, tea.Quit
+
+        // The "up" and "k" keys move the cursor up
+        case "up", "k":
+            if m.cursor > 0 {
+                m.cursor--
+            }
+
+        // The "down" and "j" keys move the cursor down
+        case "down", "j":
+            if m.cursor < len(m.tasks)-1 {
+                m.cursor++
+            }
+
+        // The "enter" key and the spacebar (a literal space) toggle
+        // the selected state for the item that the cursor is pointing at.
+        case "enter", " ":
+			if err := m.handler.HandleMarkAsCompleted(m.cursor); err != nil {
+				fmt.Println("failed to mark task as completed:", err)
+				os.Exit(1)
+			}
+        }
+    }
+
+    // Return the updated model to the Bubble Tea runtime for processing.
+    // Note that we're not returning a command.
+    return m, nil
+}
+
+func (m model) View() string {
+    // The header
+    s := "How are you today?\n\n"
+
+    // Iterate over our tasks
+    for i, task := range m.tasks {
+
+        // Is the cursor pointing at this task?
+        cursor := " " // no cursor
+        if m.cursor == i {
+            cursor = ">" // cursor!
+        }
+
+        // Is this task completed?
+        checked := " " // not selected
+        if m.tasks[i].Completed {
+            checked = "x" // selected!
+        }
+
+        // Render the row
+        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, task.Name)
+    }
+
+    // The footer
+    s += "\nPress q to quit.\n"
+
+    // Send the UI for rendering
+    return s
+}
+
+func main() {
+    p := tea.NewProgram(initialModel())
+    if _, err := p.Run(); err != nil {
+        fmt.Printf("Alas, there's been an error: %v", err)
+        os.Exit(1)
+    }
+}
